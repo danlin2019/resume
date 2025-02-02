2e21421a32b2d225c3078423ff3d55195ae2b3cc
danlin <asmob2011@gmail.com>
2025-02-02 22:39:13 +0800

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/dan/resume/' : '/dan/resume/',
+  base: process.env.NODE_ENV === 'production' ? '/resume/' : '/',
   define: {
     'process.env.API_BASE_URL': JSON.stringify(process.env.NODE_ENV === 'production'? '/dan/resume/': '/dan/resume/'),
   },
